f5a3ff611adcf652f8b6cd62a06c505e1720c942
Will Wadman <wadmanwill@gmail.com>
2022-12-14 20:20:37 +0000

    Add variables to dashboard controller to be exported to dahsbaord show, add html styling and links to the admin products page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   def show
+    @productscount = Product.count params[:id]
+    @categoriescount = Category.count 
   end
 end
